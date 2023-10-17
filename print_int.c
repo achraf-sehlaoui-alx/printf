ba52f7fad57d068b5e990e61cbbeacb8f3424f1a
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-17 16:07:44 -0700

    add some changes

--- print_int.c
+++ print_int.c
@@ -5,7 +5,7 @@
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width.
+ * @width:  width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
